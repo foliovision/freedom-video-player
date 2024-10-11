204927eeb0913478d2ea48f42c5615b24a090914
fvmartin <fvmartin@users.noreply.github.com>
2024-10-11 15:41:37 +0200

    branding: Show before starting video

--- deps/branding.js
+++ deps/branding.js
@@ -1,24 +1,20 @@
 freedomplayer(function(player, root) {
-  player.on('ready', function() {
 
-    if ( typeof player.conf.logo === 'undefined' ) return;
+  if ( typeof player.conf.logo === 'undefined' ) return;
 
-    if( root.querySelector('.fp-logo') ) root.querySelector('.fp-logo').remove();
+  var image = document.createElement("img");
+  image.src = player.conf.logo;
 
-    var image = document.createElement("img");
-    image.src = player.conf.logo;
+  var logo = document.createElement("a");
+  logo.className = 'fp-logo';
+  logo.appendChild(image);
 
-    var logo = document.createElement("a");
-    logo.className = 'fp-logo';
-    logo.appendChild(image);
+  var fp_player = root.querySelector(".fp-player");
 
-    var fp_player = root.querySelector(".fp-player");
-
-    if( fp_player ) {
-      fp_player.appendChild(logo);
-    } else {
-      root.appendChild(logo);
-    }
-  });
+  if( fp_player ) {
+    fp_player.appendChild(logo);
+  } else {
+    root.appendChild(logo);
+  }
 
 });
